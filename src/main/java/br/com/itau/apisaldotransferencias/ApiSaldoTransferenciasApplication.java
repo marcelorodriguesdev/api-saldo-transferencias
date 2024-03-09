79b4c3e7c3ae4400dac769a547b5c2a8353311f0
Marcelo Rodrigues <violontchelo@MacBook-Air-de-Marcelo.local>
2024-03-09 15:57:24 -0300

    initial commit

---
+++ src/main/java/br/com/itau/apisaldotransferencias/ApiSaldoTransferenciasApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.apisaldotransferencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSaldoTransferenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSaldoTransferenciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/itau/apisaldotransferencias/ApiSaldoTransferenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.apisaldotransferencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSaldoTransferenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
