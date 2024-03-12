577ce8d94bcd5661307214ead29b8523716885a1
Marcelo Rodrigues <violontchelo@MacBook-Air-de-Marcelo.local>
2024-03-12 10:51:15 -0300

    ajuste script sql

--- src/main/resources/db/migration/V1__Criar_tabela_transferencia_bancaria_e_saldo_conta_corrente.sql
+++ src/main/resources/db/migration/V1__Criar_tabela_transferencia_bancaria_e_saldo_conta_corrente.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS tb_transferencia_bancaria (
     num_conta_destino VARCHAR(30) NOT NULL,
     dat_horario_da_transferencia VARCHAR(255) NOT NULL,
     val_transferencia DECIMAL(19, 2) NOT NULL,
-    PRIMARY KEY (id_transferencia_bancaria)
+    PRIMARY KEY (cod_transferencia_bancaria)
 );
 
 CREATE TABLE IF NOT EXISTS tb_saldo_conta_corrente (
@@ -16,5 +16,5 @@ CREATE TABLE IF NOT EXISTS tb_saldo_conta_corrente (
     num_conta_corrente VARCHAR(30) NOT NULL,
     val_limite_disponivel DECIMAL(19, 2) NOT NULL,
     val_limite_diario DECIMAL(19, 2) NOT NULL DEFAULT 1000.00,
-    PRIMARY KEY (id_conta_corrente)
+    PRIMARY KEY (cod_conta_corrente)
 );
