1ea152d357741570f327f9c7f51221ce437d6abb
Marcelo Rodrigues <violontchelo@MacBook-Air-de-Marcelo.local>
2024-03-12 10:48:01 -0300

    adiciona campo codigo banco de destino

--- src/main/resources/db/migration/V1__Criar_tabela_transferencia_bancaria_e_saldo_conta_corrente.sql
+++ src/main/resources/db/migration/V1__Criar_tabela_transferencia_bancaria_e_saldo_conta_corrente.sql
@@ -2,8 +2,9 @@ CREATE DATABASE IF NOT EXISTS db_saldo_transferencias;
 USE db_saldo_transferencias;
 
 CREATE TABLE IF NOT EXISTS tb_transferencia_bancaria (
-    id_transferencia_bancaria CHAR(36) NOT NULL,
+    cod_transferencia_bancaria CHAR(36) NOT NULL,
     num_conta_origem VARCHAR(30) NOT NULL,
+    cod_banco_destino VARCHAR(30) NOT NULL,
     num_conta_destino VARCHAR(30) NOT NULL,
     dat_horario_da_transferencia VARCHAR(255) NOT NULL,
     val_transferencia DECIMAL(19, 2) NOT NULL,
@@ -11,7 +12,7 @@ CREATE TABLE IF NOT EXISTS tb_transferencia_bancaria (
 );
 
 CREATE TABLE IF NOT EXISTS tb_saldo_conta_corrente (
-    id_conta_corrente CHAR(36) NOT NULL,
+    cod_conta_corrente CHAR(36) NOT NULL,
     num_conta_corrente VARCHAR(30) NOT NULL,
     val_limite_disponivel DECIMAL(19, 2) NOT NULL,
     val_limite_diario DECIMAL(19, 2) NOT NULL DEFAULT 1000.00,
